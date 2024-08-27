36b4d5d803fe2a2c9af91b261b6f42bf8a8fd2ad
B.Vancel <zafkiel023@gmail.com>
2024-08-27 14:15:03 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SpeedInsights></SpeedInsights>
       <body className={inter.className}>{children}</body>
     </html>
   );
